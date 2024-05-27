3f6782a3d4e9c0a86df2e21c6bca5317d671cdd3
votuan5922 <2024802010146@student.tdmu.edu.vn>
2024-05-28 03:49:17 +0700

    bt3

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react"
+import { Appbar, Button, TextInput } from "react-native-paper"
+import { FlatList, View } from "react-native"
+import Todo from "./todo"
+import firebase from "@react-native-firebase/app"
+import firestore from "@react-native-firebase/firestore"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const App = ()=>{
+  const [todo, setTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([]);
+  const ref = firestore().collection('todos');
+  async function addTodo(){
+    await ref.add({
+      title:todo,
+      complete:false,
+    });
+    setTodo('');
+  }
+  useEffect(()=>{
+    return ref.onSnapshot(querySnapshot => {
+      const list =[];
+      querySnapshot.forEach(doc=>{
+        const {title, complete} = doc.data();
+        list.push({
+          id: doc.id,
+          title,
+          complete,
+        })
+      })
+      setTodos(list);
+      if (loading){
+        setLoading(false);
+      }
+    });
+  });
+  if (loading){
+    return null;
+  }
+  return(
+    <View style={{flex:1}}>
+      <Appbar>
+        <Appbar.Content title={' TODOs List'}/>
+      </Appbar>
+      <FlatList
+        style={{flex:1}}
+        data={todos}
+        keyExtractor={(item)=>item.id}
+        renderItem={({item})=> <Todo {...item} /> }
+      />
+      <TextInput label={'New Todo'} 
+        value={todo}
+        onChangeText={(text) =>setTodo(text)}/>
+      <Button onPress={addTodo}>Add TODO</Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import firestore from '@react-native-firebase/firestore';
+import {List} from 'react-native-paper';
+
+export default function Todo({id, title, complete}) {
+  async function toggleComplete() {
+    await firestore().collection('todos').doc(id).update({complete: !complete});
+  }
+  return (
+    <List.Item
+      title={title}
+      onPress={() => toggleComplete()}
+      left={props => (
+        <List.Icon {...props} icon={complete ? 'check' : 'cancel'} />
+      )}
+    />
+  );
+}
